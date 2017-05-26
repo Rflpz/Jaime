3e5d1eae733b658554ce66fae83a255d2afe4428
Rflpz <rflpzdev@gmail.com>
2017-05-26 09:04:04 -0500

    Xcode Template

---
+++ Jaime/Jaime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jaime
+//
+//  Created by Rafael Lopez on 5/26/17.
+//  Copyright © 2017 Rflpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
